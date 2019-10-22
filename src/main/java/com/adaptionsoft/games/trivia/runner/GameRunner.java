1561269c3f04c3cdf7189e17868494ec4206db7e
Aida Manna <aidamanna@gmail.com>
2019-10-22 23:28:36 +0200

    Add Golden Master test

--- src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
+++ src/main/java/com/adaptionsoft/games/trivia/runner/GameRunner.java
@@ -11,26 +11,26 @@ public class GameRunner {
 
 	public static void main(String[] args) {
 		Game aGame = new Game();
-		
+
 		aGame.add("Chet");
 		aGame.add("Pat");
 		aGame.add("Sue");
-		
+
 		Random rand = new Random();
-	
+
 		do {
-			
+
 			aGame.roll(rand.nextInt(5) + 1);
-			
+
 			if (rand.nextInt(9) == 7) {
 				notAWinner = aGame.wrongAnswer();
 			} else {
 				notAWinner = aGame.wasCorrectlyAnswered();
 			}
-			
-			
-			
+
+
+
 		} while (notAWinner);
-		
+
 	}
 }
---
+++ src/test/java/com/adaptionsoft/games/trivia/IntegrationTest.java
@@ -0,0 +1,62 @@
+package com.adaptionsoft.games.trivia;
+
+import com.adaptionsoft.games.uglytrivia.Game;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import org.approvaltests.Approvals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class IntegrationTest {
+
+    private ByteArrayOutputStream triviaOutput;
+    private Game game;
+    private static List<String> players = Arrays.asList("Chet", "Pat", "Sue");
+    private static Random randomNumber;
+    private static boolean notAWinner;
+
+    @Before
+    public void setUp() {
+        triviaOutput = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(triviaOutput);
+        System.setOut(printStream);
+
+        game = new Game();
+        randomNumber = new Random(100);
+    }
+
+    @Test
+    public void playUglyTriviaGame() {
+        addPlayersTo(game);
+
+        do {
+            game.roll(dice());
+            notAWinner = answerQuestion(game);
+        } while (notAWinner);
+
+        Approvals.verify(triviaOutput.toString());
+    }
+
+    private void addPlayersTo(Game game) {
+        for (String player : players) {
+            game.add(player);
+        }
+    }
+
+    private int dice() {
+        return randomNumber.nextInt(5) + 1;
+    }
+
+    private Boolean answerQuestion(Game game) {
+        if (randomNumber.nextInt(9) == 7) {
+            return game.wrongAnswer();
+        }
+
+        return game.wasCorrectlyAnswered();
+    }
+}
